41d238f029765df4c46a70248af7f8ce21ceb94d
Rene Milk <koshi@springlobby.info>
2010-01-22 14:49:11 +0100

    buildsystem

---
+++ cmake/package_config.cmake
@@ -0,0 +1,65 @@
+#http://www.cmake.org/Wiki/CMake:CPackConfiguration
+
+#don't write when git errored out resulting in unset version (ie when compiling from tarball)
+IF ( NOT GIT_ERROR )
+    CONFIGURE_FILE( ${wxmapconv_SOURCE_DIR}/cmake/config.h ${wxmapconv_BINARY_DIR}/config.h )
+ENDIF ( NOT GIT_ERROR )
+
+SET( PACKAGE_NAME wxmapconv )
+SET( PACKAGE_VERSION ${wxmapconv_REV} )
+
+IF (WIN32)
+    SET(CPACK_GENERATOR "ZIP")
+    SET(CPACK_PACKAGE_FILE_NAME "wxmapconv-${wxmapconv_REV}-win32")
+	FILE( GLOB wxdlls "${wxWidgets_LIB_DIR}/wx*.dll" )
+	FOREACH ( file ${wxdlls} )
+		INSTALL(FILES ${file} DESTINATION . )
+ 	ENDFOREACH( file )
+# 	FOREACH ( file  )
+# 		INSTALL(FILES ${wxWidgets_LIB_DIR}/${file}.dll DESTINATION . )
+#  	ENDFOREACH( file )
+	SET( BASEPATH /opt/mingw32/ )
+	INSTALL(FILES
+		${BASEPATH}/i586-pc-mingw32/bin/mingwm10.dll
+		DESTINATION . )
+	
+ELSE (WIN32)
+    SET(CPACK_CMAKE_GENERATOR "Unix Makefiles")
+    SET(CPACK_GENERATOR "TBZ2;TGZ")
+    SET(CPACK_PACKAGE_FILE_NAME "wxmapconv-${wxmapconv_REV}")
+	IF( EXISTS ${wxmapconv_BINARY_DIR}/config.h )
+		INSTALL(FILES
+			${wxmapconv_BINARY_DIR}/config.h
+			DESTINATION . OPTIONAL)
+	ENDIF( EXISTS ${wxmapconv_BINARY_DIR}/config.h )
+ENDIF (WIN32)
+SET(CPACK_INSTALL_CMAKE_PROJECTS "${CMAKE_BINARY_DIR};${CMAKE_PROJECT_NAME};ALL;/")
+# SET(CPACK_OUTPUT_CONFIG_FILE "/home/andy/vtk/CMake-bin/CPackConfig.cmake")
+# SET(CPACK_PACKAGE_DESCRIPTION_FILE "/home/andy/vtk/CMake/Copyright.txt")
+# SET(CPACK_PACKAGE_DESCRIPTION_SUMMARY "CMake is a build tool")
+#SET(CPACK_PACKAGE_EXECUTABLES "wxmapconv" "springsettings")
+SET(CPACK_PACKAGE_INSTALL_DIRECTORY "wxmapconv")
+SET(CPACK_PACKAGE_NAME "wxmapconv")
+SET(CPACK_PACKAGE_VENDOR "The SpringLobby Team")
+SET(CPACK_PACKAGE_VERSION ${wxmapconv_REV})
+
+SET(CPACK_RESOURCE_FILE_LICENSE ${wxmapconv_SOURCE_DIR}/COPYING)
+SET(CPACK_RESOURCE_FILE_README ${wxmapconv_SOURCE_DIR}/README)
+# SET(CPACK_RESOURCE_FILE_WELCOME "/home/andy/vtk/CMake/Templates/CPack.GenericWelcome.txt")
+SET(CPACK_SOURCE_GENERATOR "TGZ;TBZ2")
+# SET(CPACK_SOURCE_OUTPUT_CONFIG_FILE "/home/andy/vtk/CMake-bin/CPackSourceConfig.cmake")
+SET(CPACK_SOURCE_PACKAGE_FILE_NAME "wxmapconv-${wxmapconv_REV}")
+SET(CPACK_STRIP_FILES TRUE)
+SET(CPACK_SOURCE_STRIP_FILES TRUE)
+# SET(CPACK_SYSTEM_NAME "Linux-i686")
+# SET(CPACK_TOPLEVEL_TAG "Linux-i686")
+set(CPACK_SOURCE_IGNORE_FILES
+"^${wxmapconv_SOURCE_DIR}/build*"
+"^${wxmapconv_SOURCE_DIR}/bin/"
+"^${wxmapconv_SOURCE_DIR}/.*"
+"^${wxmapconv_SOURCE_DIR}/auto*"
+"^${wxmapconv_SOURCE_DIR}/doc/"
+"^${wxmapconv_SOURCE_DIR}/m4/"
+"^${wxmapconv_SOURCE_DIR}/obj/"
+)
+INCLUDE(CPack)
